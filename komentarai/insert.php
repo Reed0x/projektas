1a0bd10aa25ba5ec27804679f936ba642a9cda21
Asus <Asus@VYLE>
2017-05-30 20:03:50 +0300

    naujausia versija, su keistomis spavomis, nuimtais nereikalingais mygtukais, nedadaryta sasaja destytojai>>>>FACEBOOK

---
+++ komentarai/insert.php
@@ -0,0 +1,44 @@
+<?php 
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "mano";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Nepavyko prisijungti spausk ALT+F4: " . $conn->connect_error);
+} 
+echo "Pavyko prisijungti spausk ALT+F4"; 
+
+$sql = "INSERT INTO comments ( 
+				article_id, 
+				page, 
+				username, 
+				subject, 
+				contact, 
+				comment, 
+				ip, 
+				date, 
+				time)
+		VALUES ('".$_POST["article_id"]."', 
+				'komentarai', 
+				'".$_POST["username"]."',
+				'',
+				'',
+				'".$_POST["comment"]."',
+				'',
+				Now(),
+				'')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Duomenys sukurti";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}				
+
+$conn->close();
+
+header('Location: ../index.php');
+
+?>
